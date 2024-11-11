16be6122ceeb0ae3ba50c131a96418eec3060288
tshwanelo <t.matsane@gmail.com>
2024-11-11 07:24:35 +0200

    Completed my first programming exercise

--- 11-001 Your First Computer Program/11-001 Your First Computer Program/Greeting Example/greeting.js
+++ 11-001 Your First Computer Program/11-001 Your First Computer Program/Greeting Example/greeting.js
@@ -1,1 +1,8 @@
 // Code here for Compulsory Task 1
+
+let userName = prompt("Enter your name: ");
+console.log(userName);
+let userAge = prompt("Enter your age: ");
+console.log(userAge);
+
+console.log(`\nHello, ${userName}, it's nice to meet you!`);
--- 11-001 Your First Computer Program/11-001 Your First Computer Program/Shopping Example/shopping.js
+++ 11-001 Your First Computer Program/11-001 Your First Computer Program/Shopping Example/shopping.js
@@ -1,1 +1,7 @@
 // Code here for Compulsory Task 2
+
+let item1 = prompt("Input shopping item 1: ");
+let item2 = prompt("Input shopping item 2: ");
+let item3 = prompt("Input shopping item 3: ");
+
+console.log(`My Shopping list:\n${item1}\n${item2}\n${item3}`);
